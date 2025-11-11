0d8ffff90dab44abe4b2e8c79c69e82067e6a276
Oluremi Oladipupo <oluremioladipupo2007@gmail.com>
2025-11-11 21:57:46 +0100

    banner imgs(slide) and media queries

---
+++ script.js
@@ -0,0 +1,18 @@
+// ==== HERO SLIDE =====
+let slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) slide.classList.add("active");
+  });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+// auto slide every 2s
+setInterval(nextSlide, 3000);
